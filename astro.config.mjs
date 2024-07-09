548bd7d1e354170efcc03417b818bc4394718edf
Robin Bühler <git+r@obin.ch>
2024-07-09 14:08:14 +0000

    chore: fix cd workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale: "en",
     locales: ["de", "en"]
   },
-  integrations: [react()]
+  integrations: []
 });
