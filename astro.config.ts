341951f4f25606d8974419244a1beaaefafb5d59
Robin Bühler <git+r@obin.ch>
2024-07-14 15:16:17 +0000

    fix: using config when I shouldn't

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+import { C } from './src/configuration';
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   i18n: {
-    defaultLocale: "en",
-    locales: ["de", "en"]
+    defaultLocale: C.DEFAULT_LOCALE,
+    locales: Object.keys(C.LOCALES),
   },
   integrations: [mdx()]
 });
