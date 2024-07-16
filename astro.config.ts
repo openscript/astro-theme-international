a507292d3e0ab82d8248acc6a0a0cda5733bd30b
Robin Bühler <git+r@obin.ch>
2024-07-16 10:12:45 +0000

    feat: search

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import { C } from './src/configuration';
-import pagefind from 'astro-pagefind';
+import search from './src/integrations/search';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +13,5 @@ export default defineConfig({
     defaultLocale: C.DEFAULT_LOCALE,
     locales: Object.keys(C.LOCALES),
   },
-  integrations: [mdx(), pagefind()]
+  integrations: [mdx(), search()]
 });
