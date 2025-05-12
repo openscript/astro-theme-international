11179afb9f1f1b40a1ff4b626e1eff0f60dea202
Robin <git+r@obin.ch>
2025-05-12 17:12:34 +0000

    chore: clean up unused code

--- src/utils/i18n.test.ts
+++ src/utils/i18n.test.ts
@@ -1,16 +1,9 @@
 import { describe, it, expect, vi } from "vitest";
-import { getEntry } from "astro:content";
 import {
-  getContentEntryPath,
   getFullLocale,
-  getLocaleFromUrl,
   getMessage,
   getNameFromLocale,
-  makeMenu,
   parseLocale,
-  parseLocaleFromPath,
-  splitCollectionAndSlug,
-  splitLocaleAndPath,
   useTranslations,
 } from "../utils/i18n";
 
@@ -88,118 +81,10 @@ vi.mock("astro:content", () => ({
   },
 }));
 
-describe("parseLocaleFromPath", () => {
-  it("should parse the locale from a URL", () => {
-    const locale = parseLocaleFromPath("/en-US/docs/getting-started");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should parse the locale from a URL without leading slash", () => {
-    const locale = parseLocaleFromPath("en-US/docs/getting-started");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should parse the locale from a URL with trailing slash", () => {
-    const locale = parseLocaleFromPath("/en-US/");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should parse the locale from a URL without trailing slashes", () => {
-    const locale = parseLocaleFromPath("/en-US");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should parse the short form locale from a URL", () => {
-    const locale = parseLocaleFromPath("/en");
-    expect(locale).toBe("en");
-  });
-
-  it("should return undefined if no locale was found in URL", () => {
-    const locale = parseLocaleFromPath("/");
-    expect(locale).toBe(undefined);
-  });
-
-  it("shouldn't work on non locales", () => {
-    const locale = parseLocaleFromPath("/docs");
-    expect(locale).toBe(undefined);
-  });
-
-  it("shouldn't work on locales in file names", () => {
-    const locale = parseLocaleFromPath("/some/path/example.en-US.md");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should parse the locale from a file name", () => {
-    const locale = parseLocaleFromPath("example.en.md");
-    expect(locale).toBe("en");
-  });
-
-  it("should parse the locale from a file name with dialect", () => {
-    const locale = parseLocaleFromPath("example.en-US.md");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should parse the locale from a file with path", () => {
-    const locale = parseLocaleFromPath("/some/path/example.en-US.md");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should parse the locale from a file with relative path", () => {
-    const locale = parseLocaleFromPath("../example.en-US.md");
-    expect(locale).toBe("en-US");
-  });
-
-  it("should undefined if no locale was found in file name", () => {
-    const locale = parseLocaleFromPath("example.md");
-    expect(locale).toBe(undefined);
-  });
-});
-
-describe("splitLocaleAndPath", () => {
-  it("should split the locale from path", () => {
-    const splitPath = splitLocaleAndPath("/en-US/docs/getting-started");
-    expect(splitPath?.locale).toBe("en-US");
-    expect(splitPath?.path).toBe("/docs/getting-started");
-  });
-
-  it("should split the locale from path without leading slash", () => {
-    const splitPath = splitLocaleAndPath("en-US/docs/getting-started");
-    expect(splitPath?.locale).toBe("en-US");
-    expect(splitPath?.path).toBe("docs/getting-started");
-  });
-
-  it("should split the locale from file path", () => {
-    const splitPath = splitLocaleAndPath("/docs/getting-started.en-US.md");
-    expect(splitPath?.locale).toBe("en-US");
-    expect(splitPath?.path).toBe("/docs/getting-started");
-  });
-});
-
-describe("splitCollectionAndSlug", () => {
-  it("should split the collection from slug", () => {
-    const splitPath = splitCollectionAndSlug("pages://en/data-protection");
-    expect(splitPath.collection).toMatchInlineSnapshot(`"pages"`);
-    expect(splitPath.slug).toMatchInlineSnapshot(`"en/data-protection"`);
-  });
-
-  it("should split the locale from path without leading slash", () => {
-    expect(() =>
-      splitCollectionAndSlug("somethingElse"),
-    ).toThrowErrorMatchingInlineSnapshot(`[Error: Couldn't split path.]`);
-  });
-});
-
 describe("getNameFromLocale", () => {
   it("should return the name of the locale", () => {
     expect(getNameFromLocale("en")).toBe("English");
     expect(getNameFromLocale("de")).toBe("Deutsch");
-  });
-});
-
-describe("getLocaleFromUrl", () => {
-  it("should return the locale from a URL", () => {
-    const url = new URL("https://example.com/en-US/docs/getting-started");
-    expect(getLocaleFromUrl(url)).toBe("en");
   });
 });
 
@@ -234,65 +119,6 @@ describe("getMessage", () => {
   });
   it("should return the message", () => {
     expect(getMessage("language", "en")).toBe("English");
-  });
-});
-
-describe("getContentEntryPath", () => {
-  it("should throw an error if content entry not found", async () => {
-    const entry = await getEntry("docs" as any, "invalid");
-    expect(() => getContentEntryPath(entry)).toThrow(
-      "Entry not found",
-    );
-  });
-  it("should throw an error if entry has no international path", async () => {
-    const entry = await getEntry("docs" as any, "no-international-path");
-    expect(() => getContentEntryPath(entry)).toThrow(
-      "Entry has no international path: docs/2020/09/11/test-article.md",
-    );
-  });
-  it("should return the content entry path", async () => {
-    const entry = await getEntry("docs" as any, "getting-started")
-    const path = getContentEntryPath(entry);
-    expect(path).toMatchInlineSnapshot(`"/de/doku/2020/09/11/test-article"`);
-  });
-  it("should return the content entry path without double slug", async () => {
-    const entry = await getEntry("docs" as any, "root");
-    const path = getContentEntryPath(entry);
-    expect(path).toMatchInlineSnapshot(`"/de/doku/test-article"`);
-  });
-});
-
-describe("makeMenu", () => {
-  it("should return the menu", async () => {
-    const menu = await makeMenu("en", [
-      {
-        title: "License",
-        path: async () => getContentEntryPath(await getEntry("docs" as any, "getting-started")),
-      },
-      { title: "Privacy", path: "privacy" },
-      { title: "Terms", path: "terms" },
-      { title: "Contact", path: "contact" },
-    ]);
-    expect(menu).toMatchInlineSnapshot(`
-      [
-        {
-          "path": "/de/doku/2020/09/11/test-article",
-          "title": "License",
-        },
-        {
-          "path": "/privacy",
-          "title": "Privacy",
-        },
-        {
-          "path": "/terms",
-          "title": "Terms",
-        },
-        {
-          "path": "/contact",
-          "title": "Contact",
-        },
-      ]
-    `);
   });
 });
 
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,52 +1,8 @@
-import {
-  type CollectionEntry,
-  type CollectionKey,
-} from "astro:content";
 import { C, type Locale } from "../configuration";
-import { dirname, joinPath } from "./path";
-import slug from "limax";
-
-const PATH_LOCALE_PATTERN = /^\/?(?<locale>\w{2}(?!\w)(-\w{1,})*)\/?(?<path>.*)?/;
-const FILE_LOCALE_PATTERN = /^(?<path>.*)\.(?<locale>\w{2}(?!\w)(-\w{1,})*)\./;
-const LOCALE_PATTERNS = [PATH_LOCALE_PATTERN, FILE_LOCALE_PATTERN];
-export const PROTOCOL_DELIMITER = "://";
-
-export function splitLocaleAndPath(path: string) {
-  const pattern = LOCALE_PATTERNS.find((p) => path.match(p));
-  if (!pattern) return undefined;
-
-  const match = path.match(pattern);
-  if (!match || !match.groups || !match.groups.locale || !match.groups.path) return undefined;
-
-  const l = match.groups.locale;
-  let p = match.groups.path;
-  if (path.startsWith("/") && !p.startsWith("/")) p = `/${p}`;
-
-  return { locale: l, path: p };
-}
-
-export function splitCollectionAndSlug(path: string) {
-  const split = path.split(PROTOCOL_DELIMITER);
-
-  if (!split[0] || !split[1]) throw new Error("Couldn't split path.");
-
-  return { collection: split[0], slug: split[1] };
-}
 
 export function getNameFromLocale(locale?: string) {
   const l = parseLocale(locale);
   return C.MESSAGES[l].language;
-}
-
-export function getLocaleFromUrl(url: URL) {
-  let path = url.pathname;
-
-  if (url.pathname.startsWith(import.meta.env.BASE_URL))
-    path = url.pathname.slice(import.meta.env.BASE_URL.length);
-
-  const locale = parseLocaleFromPath(path);
-
-  return parseLocale(locale);
 }
 
 export function parseLocale(locale?: string) {
@@ -67,27 +23,6 @@ export function getMessage(key: string, locale: Locale) {
   const k = key as keyof (typeof C.MESSAGES)[typeof locale];
 
   return C.MESSAGES[locale][k];
-}
-
-export async function makeMenu(
-  locale: keyof typeof C.LOCALES,
-  items: {
-    title: string;
-    path: ((locale: keyof typeof C.LOCALES) => Promise<string>) | string;
-  }[],
-) {
-  return Promise.all(
-    items.map(async (item) => {
-      const urlLocale = locale === C.DEFAULT_LOCALE ? "" : locale;
-      const path = typeof item.path === "string"
-          ? `/${[urlLocale, item.path].filter(Boolean).join("/")}`
-          : await item.path(locale);
-      return {
-        title: item.title,
-        path,
-      };
-    }),
-  );
 }
 
 export function useTranslations<L extends keyof typeof C.MESSAGES>(locale: L) {
